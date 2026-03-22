db844fb34a178a38d40fbddb562801693f20e523
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2026-03-22 16:25:44 +0900

    registerBean 메서드

--- gsws_ap_b_ex1/src/main/java/org/example/Main.java
+++ gsws_ap_b_ex1/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -21,5 +23,13 @@ public class Main {
 
         Integer n = context.getBean(Integer.class);
         System.out.println(n);
+
+        Parrot x = new Parrot();
+        x.setName("Kiki");
+        Supplier<Parrot> parrotSupplier = () -> x;
+        context.registerBean("parrot1", Parrot.class, parrotSupplier);
+
+        Parrot p = context.getBean("parrot1", Parrot.class);
+        System.out.println(p.getName());
     }
 }
