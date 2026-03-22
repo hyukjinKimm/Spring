ecc38bc080c585b55723cb9541e2765774df10bb
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2026-03-22 16:17:25 +0900

    Component 어노테이션

--- gsws_ap_b_ex1/src/main/java/org/example/Main.java
+++ gsws_ap_b_ex1/src/main/java/org/example/Main.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p1 = context.getBean("parrot1", Parrot.class);
-        System.out.println(p1.getName());
+//        Parrot p1 = context.getBean("parrot1", Parrot.class);
+//        System.out.println(p1.getName());
+//
+//        Parrot p2 = context.getBean("parrot2", Parrot.class);
+//        System.out.println(p2.getName());
 
-        Parrot p2 = context.getBean("parrot2", Parrot.class);
-        System.out.println(p2.getName());
-
-        Parrot p3 = context.getBean("riki", Parrot.class);
+        Parrot p3 = context.getBean(Parrot.class);
         System.out.println(p3.getName());
 
         String s = context.getBean(String.class);
--- gsws_ap_b_ex1/src/main/java/org/example/ProjectConfig.java
+++ gsws_ap_b_ex1/src/main/java/org/example/ProjectConfig.java
@@ -1,29 +1,31 @@
 package org.example;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
 
-    @Bean
-    Parrot parrot1() {
-        var p = new Parrot();
-        p.setName("koko");
-        return p;
-    }
-    @Bean
-    Parrot parrot2() {
-        var p = new Parrot();
-        p.setName("Miki");
-        return p;
-    }
-    @Bean(name="riki")
-    Parrot parrot3() {
-        var p = new Parrot();
-        p.setName("Riki");
-        return p;
-    }
+//    @Bean
+//    Parrot parrot1() {
+//        var p = new Parrot();
+//        p.setName("koko");
+//        return p;
+//    }
+//    @Bean
+//    Parrot parrot2() {
+//        var p = new Parrot();
+//        p.setName("Miki");
+//        return p;
+//    }
+//    @Bean(name="riki")
+//    Parrot parrot3() {
+//        var p = new Parrot();
+//        p.setName("Riki");
+//        return p;
+//    }
 
     @Bean
     String hello(){
