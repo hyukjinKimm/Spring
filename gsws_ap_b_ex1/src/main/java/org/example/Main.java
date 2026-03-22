3583c8990eee5925eb1e40948533a4baa04edec1
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2026-03-22 16:04:37 +0900

    Bean 추가

---
+++ gsws_ap_b_ex1/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot p1 = context.getBean("parrot1", Parrot.class);
+        System.out.println(p1.getName());
+
+        Parrot p2 = context.getBean("parrot2", Parrot.class);
+        System.out.println(p2.getName());
+
+        Parrot p3 = context.getBean("riki", Parrot.class);
+        System.out.println(p3.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
+    }
+}
---
+++ gsws_ap_b_ex1/src/main/java/org/example/ProjectConfig.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot1() {
+        var p = new Parrot();
+        p.setName("koko");
+        return p;
+    }
+    @Bean
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+    @Bean(name="riki")
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Riki");
+        return p;
+    }
+
+    @Bean
+    String hello(){
+        return "Hello";
+    }
+
+    @Bean
+    Integer ten(){
+        return 10;
+    }
+}
