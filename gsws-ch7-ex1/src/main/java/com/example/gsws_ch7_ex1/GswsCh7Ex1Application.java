5d3bfb3def3033046eb44eafb966778edf2401f3
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2026-04-19 12:39:27 +0900

    tomcat

---
+++ gsws-ch7-ex1/src/main/java/com/example/gsws_ch7_ex1/GswsCh7Ex1Application.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ch7_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GswsCh7Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GswsCh7Ex1Application.class, args);
+	}
+
+}
